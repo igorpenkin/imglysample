7c8faeeac2f1fd9cb5f5527be2532e0fd1ab42f6
Igor Penkin <penkin.igor@gmail.com>
2022-04-27 17:42:49 +0300

    Empty project

---
+++ imglysample/imglysample/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  imglysample
+//
+//  Created by Igor Penkin on 27.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .secondarySystemBackground
+    }
+}
+
